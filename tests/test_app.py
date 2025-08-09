764af1c8f81287fbaf1f4c9fd4a09fae6bb661a3
Pedro Dutra <pedrodutra86@hotmail.com>
2025-08-09 19:47:48 -0300

    feat: creating repo

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+
+def test_root_must_return_hello_world():
+    client = TestClient(app)
+
+    response = client.get('/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Hello World!'}
