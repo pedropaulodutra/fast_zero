25781ea3c8b17d937a1033d92bf630f5f1f7485b
Pedro Dutra <pedrodutra86@hotmail.com>
2025-08-11 00:07:32 -0300

    feat: create schemas, a new endpoint /home and test it

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,27 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+from fast_zero.schemas import Message
+
+app = FastAPI(title='Task Manager')
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello World!'}
+
+
+@app.get('/home', response_class=HTMLResponse)
+def home():
+    return """
+    <html>
+        <head>
+            <title>Hello World!</title>
+        </head>
+        <body>
+            <h1>Hello World!</h1>
+        </body>
+    </html>
+"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -5,10 +5,17 @@ from fastapi.testclient import TestClient
 from fast_zero.app import app
 
 
-def test_root_must_return_hello_world():
+def test_root_must_return_200():
     client = TestClient(app)
 
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Hello World!'}
+
+
+def test_home_should_return_hello_world():
+    client = TestClient(app)
+
+    response = client.get('/home')
+    assert response.status_code == HTTPStatus.OK
+    assert 'Hello World!' in response.text
